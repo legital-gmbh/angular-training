6825ec35a2c05984780da048aa51d6e47c62537c
Lennart Gamradt <lennart.gamradt@nordakademie.de>
2022-12-03 13:47:07 +0100

    2-solution-modules-components-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'room',
+    loadChildren: () => import('./room/room.module').then(m => m.RoomModule)
+  },
+  { path: 'hello', component: HelloComponent },
+  { path: '', redirectTo: '/hello', pathMatch: 'full' },
+  { path: '**', component: HelloComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { FormsModule } from '@angular/forms';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
